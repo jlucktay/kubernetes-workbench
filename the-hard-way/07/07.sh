5d087468427db92460f391a5fe2b03d5d7cc6a6a
James Lucktaylor <jlucktay+github@gmail.com>
2018-12-12 13:49:44 +0000

    Completed section 7

--- the-hard-way/07/07.sh
+++ the-hard-way/07/07.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-# gcloud compute ssh controller-0
-# gcloud compute ssh controller-1
-# gcloud compute ssh controller-2
-
 for instance in controller-0 controller-1 controller-2; do
     gcloud compute scp 07-controller.sh ${instance}:~/
 done
+
+gcloud compute ssh controller-0
+gcloud compute ssh controller-1
+gcloud compute ssh controller-2
