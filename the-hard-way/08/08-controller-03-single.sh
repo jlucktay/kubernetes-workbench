2f2cb8fb39383ae4c597232e714c8750e428d65e
James Lucktaylor <jlucktay+github@gmail.com>
2018-12-12 14:19:20 +0000

    WIP section 8

---
+++ the-hard-way/08/08-controller-03-single.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+### In this section you are interacting with your cluster as a whole, so the following 2 role creation commands only need to be run from a single controller
+
+# Create the system:kube-apiserver-to-kubelet ClusterRole with permissions to access the Kubelet API and perform most common tasks associated with managing pods
+cat <<EOF | kubectl apply --kubeconfig admin.kubeconfig -f -
+apiVersion: rbac.authorization.k8s.io/v1beta1
+kind: ClusterRole
+metadata:
+  annotations:
+    rbac.authorization.kubernetes.io/autoupdate: "true"
+  labels:
+    kubernetes.io/bootstrapping: rbac-defaults
+  name: system:kube-apiserver-to-kubelet
+rules:
+  - apiGroups:
+      - ""
+    resources:
+      - nodes/proxy
+      - nodes/stats
+      - nodes/log
+      - nodes/spec
+      - nodes/metrics
+    verbs:
+      - "*"
+EOF
+
+# Bind the system:kube-apiserver-to-kubelet ClusterRole to the kubernetes user
+cat <<EOF | kubectl apply --kubeconfig admin.kubeconfig -f -
+apiVersion: rbac.authorization.k8s.io/v1beta1
+kind: ClusterRoleBinding
+metadata:
+  name: system:kube-apiserver
+  namespace: ""
+roleRef:
+  apiGroup: rbac.authorization.k8s.io
+  kind: ClusterRole
+  name: system:kube-apiserver-to-kubelet
+subjects:
+  - apiGroup: rbac.authorization.k8s.io
+    kind: User
+    name: kubernetes
+EOF
--- the-hard-way/08/08.sh
+++ the-hard-way/08/08.sh
@@ -1,9 +1,48 @@
 #!/usr/bin/env bash
 
-for instance in controller-0 controller-1 controller-2; do
-    gcloud compute scp 08-controller-01.sh 08-controller-02.sh ${instance}:~/
-done
+# for instance in controller-0 controller-1 controller-2; do
+#     gcloud compute scp 08-controller-01.sh 08-controller-02.sh ${instance}:~/
+# done
 
 # gcloud compute ssh controller-0
 # gcloud compute ssh controller-1
 # gcloud compute ssh controller-2
+
+# gcloud compute scp 08-controller-03-single.sh controller-0:~/
+
+# gcloud compute ssh controller-0
+
+# {
+#     KUBERNETES_PUBLIC_ADDRESS=$(gcloud compute addresses describe kubernetes-the-hard-way \
+#         --region $(gcloud config get-value compute/region) \
+#         --format 'value(address)')
+
+#     gcloud compute http-health-checks create kubernetes \
+#         --description "Kubernetes Health Check" \
+#         --host "kubernetes.default.svc.cluster.local" \
+#         --request-path "/healthz"
+
+#     gcloud compute firewall-rules create kubernetes-the-hard-way-allow-health-check \
+#         --network kubernetes-the-hard-way \
+#         --source-ranges 209.85.152.0/22,209.85.204.0/22,35.191.0.0/16 \
+#         --allow tcp
+
+#     gcloud compute target-pools create kubernetes-target-pool \
+#         --http-health-check kubernetes
+
+#     gcloud compute target-pools add-instances kubernetes-target-pool \
+#         --instances controller-0,controller-1,controller-2
+
+#     gcloud compute forwarding-rules create kubernetes-forwarding-rule \
+#         --address ${KUBERNETES_PUBLIC_ADDRESS} \
+#         --ports 6443 \
+#         --region $(gcloud config get-value compute/region) \
+#         --target-pool kubernetes-target-pool
+# }
+
+KUBERNETES_PUBLIC_ADDRESS=$(gcloud compute addresses describe kubernetes-the-hard-way \
+    --region $(gcloud config get-value compute/region) \
+    --format 'value(address)')
+
+# Make a HTTP request for the Kubernetes version info
+curl --cacert ca.pem https://${KUBERNETES_PUBLIC_ADDRESS}:6443/version
