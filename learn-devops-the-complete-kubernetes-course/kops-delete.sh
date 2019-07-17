83980542a50813d56231513d0dc238fd14616680
James Lucktaylor <jlucktay+github@gmail.com>
2019-07-17 13:31:15 +0100

    Add 2x helper scripts.

---
+++ learn-devops-the-complete-kubernetes-course/kops-delete.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# Thank you: https://github.com/anordal/shellharden/blob/master/how_to_do_things_safely_in_bash.md#how-to-begin-a-bash-script
+if test "$BASH" = "" || "$BASH" -uc "a=();true \"\${a[@]}\"" 2>/dev/null; then
+    # Bash 4.4, Zsh
+    set -euo pipefail
+else
+    # Bash 4.3 and older chokes on empty arrays with set -u.
+    set -eo pipefail
+fi
+shopt -s nullglob globstar
+IFS=$'\n\t'
+
+ScriptDirectory="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+# shellcheck source=./export.sh
+. "$(realpath "$ScriptDirectory/export.sh")"
+
+kops delete cluster --name=$CLUSTER_NAME --state=$KOPS_STATE_STORE --yes
