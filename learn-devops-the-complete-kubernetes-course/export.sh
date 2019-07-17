f2e064ad0524bad4974e1a381a5cb052192fb6f6
James Lucktaylor <jlucktay+github@gmail.com>
2019-07-17 16:12:49 +0100

    Resequence zones.

--- learn-devops-the-complete-kubernetes-course/export.sh
+++ learn-devops-the-complete-kubernetes-course/export.sh
@@ -4,7 +4,7 @@ export CLOUD="gce"
 export CLUSTER_NAME="simple.k8s.local"
 export KOPS_STATE_STORE="gs://k8s-kops-state"
 export PROJECT="cr-lab-jlucktaylor-1707191144"
-export ZONES="europe-west2-c,europe-west2-b,europe-west2-a"
+export ZONES="europe-west2-a,europe-west2-b,europe-west2-c"
 
 # X nodes per zone
 ZONE_COUNT_INTERMEDIATE="${ZONES//[^,]}"
