50f34092e3645f726482527baa9630c8f0981bfa
James Lucktaylor <jlucktay+github@gmail.com>
2019-07-17 16:13:30 +0100

    Confirm create on first run.

--- learn-devops-the-complete-kubernetes-course/kops-create.sh
+++ learn-devops-the-complete-kubernetes-course/kops-create.sh
@@ -15,4 +15,5 @@ ScriptDirectory="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 # shellcheck source=./export.sh
 . "$(realpath "$ScriptDirectory/export.sh")"
 
-kops create cluster --name=$CLUSTER_NAME --cloud=$CLOUD --state=$KOPS_STATE_STORE --project=$PROJECT --zones=$ZONES --node-count=$NODE_COUNT
+kops create cluster --name=$CLUSTER_NAME --cloud=$CLOUD --state=$KOPS_STATE_STORE --project=$PROJECT --zones=$ZONES \
+    --node-count=$NODE_COUNT --yes
