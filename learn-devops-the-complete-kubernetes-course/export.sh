a887a02f50c9ea3ec95bf0bcc05e38693b4ca047
James Lucktaylor <jlucktay+github@gmail.com>
2019-07-17 13:22:26 +0100

    Update for new lab project.

--- learn-devops-the-complete-kubernetes-course/export.sh
+++ learn-devops-the-complete-kubernetes-course/export.sh
@@ -1,6 +1,6 @@
 export CLUSTER_NAME="simple.k8s.local"
-export KOPS_STATE_STORE="gs://k8s-kops-state-0105193546/"
-export PROJECT="cr-lab-jlucktaylor-0105193546"
+export KOPS_STATE_STORE="gs://k8s-kops-state"
+export PROJECT="cr-lab-jlucktaylor-1707191144"
 export ZONES="europe-west2-c,europe-west2-b,europe-west2-a"
 export NODE_COUNT="2"
 
