1228fae724863389f7c9d119cf2be91784fcb0f3
James Lucktaylor <jlucktay+github@gmail.com>
2019-08-07 17:01:35 +0100

    Explicitly specify cloud platform.

--- learn-devops-the-complete-kubernetes-course/aws/kops-create.sh
+++ learn-devops-the-complete-kubernetes-course/aws/kops-create.sh
@@ -15,4 +15,4 @@ ScriptDirectory="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 # shellcheck source=./export.sh
 . "$(realpath "$ScriptDirectory/export.sh")"
 
-kops create cluster --zones="$ZONES" $CLUSTER_NAME --node-count=$NODE_COUNT
+kops create cluster --cloud="$CLOUD" --zones="$ZONES" $CLUSTER_NAME --node-count=$NODE_COUNT
