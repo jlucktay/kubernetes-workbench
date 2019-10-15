62a1e1e9077cb08fa862858b19fa23db4fee0930
James Lucktaylor <jlucktay+github@gmail.com>
2019-10-15 12:46:53 +0100

    Sort out expansion of ZONES array.

--- learn-devops-the-complete-kubernetes-course/aws/kops-create.sh
+++ learn-devops-the-complete-kubernetes-course/aws/kops-create.sh
@@ -31,7 +31,9 @@ done
 . "$(realpath "$ScriptDirectory/export.sh")" --kops
 
 # kops create cluster --cloud="$CLOUD" --zones="$ZONES" $CLUSTER_NAME --node-count=$NODE_COUNT --dry-run --output json
-KopsArgs=(create cluster "--cloud=$CLOUD" "--zones=${ZONES[*]}" "$CLUSTER_NAME" "--node-count=$NODE_COUNT")
+KopsArgs=(create cluster "--cloud=$CLOUD")
+IFS="," KopsArgs+=("--zones=${ZONES[*]}")
+KopsArgs+=("--node-count=$NODE_COUNT" "$CLUSTER_NAME")
 
 if [ "$YES" == 1 ]; then
     KopsArgs+=(--yes)
