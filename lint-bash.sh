edc95d106bc9ea5efa18c9b69abd48d9db32b73b
James Lucktaylor <jlucktay+github@gmail.com>
2020-12-01 09:36:46 +0000

    build(linters): add script

---
+++ lint-bash.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+### NOTES
+# Linters called by this script are (where possible) configured to follow Google's shell style guide:
+# https://google.github.io/styleguide/shell.xml
+
+readonly REPO_ROOT="$(cd "$(dirname "${BASH_SOURCE[-1]}")" &> /dev/null && git rev-parse --show-toplevel)"
+
+# https://github.com/mvdan/sh
+find -x "$REPO_ROOT" -type f -iname "*sh" \
+  -not -path "*/.git/*" \
+  -print0 \
+  | xargs -0 -n 1 shfmt -w -s -ln=bash -i=2 -bn -ci -sr
+
+# https://github.com/anordal/shellharden
+find -x "$REPO_ROOT" -type f -iname "*sh" \
+  -not -path "*/.git/*" \
+  -print0 \
+  | xargs -0 -n 1 shellharden --check --replace --
+
+# https://github.com/koalaman/shellcheck
+find -x "$REPO_ROOT" -type f -iname "*sh" \
+  -not -path "*/.git/*" \
+  -print0 \
+  | xargs -0 -n 1 shellcheck --enable=all --exclude=SC2250 --severity=style --shell=bash --external-sources --
+
+# Exclude: https://www.shellcheck.net/wiki/SC2250
+# Prefer putting braces around variable references even when not strictly required.
