a7d4719ca1c39dba1dd4b2ac9f2362eed56ee25f
James Lucktaylor <jlucktay+github@gmail.com>
2025-08-07 11:38:44 +0100

    build(aoc): upgrade to 1.32

--- scripts/task.cluster-create.sh
+++ scripts/task.cluster-create.sh
@@ -5,11 +5,11 @@ k3d cluster create --config="${GIT_ROOT:?}/k3d-config.yaml"
 
 # Prime the new cluster with the images it will use.
 declare -a k3d_images=(
-  rancher/klipper-helm:v0.9.7-build20250616
+  rancher/klipper-helm:v0.9.8-build20250709
   rancher/klipper-lb:v0.4.13
   rancher/local-path-provisioner:v0.0.31
   rancher/mirrored-coredns-coredns:1.12.1
-  rancher/mirrored-library-traefik:2.11.24
+  rancher/mirrored-library-traefik:3.3.6
   rancher/mirrored-metrics-server:v0.7.2
 )
 
