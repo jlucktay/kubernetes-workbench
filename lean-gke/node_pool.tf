92010f6a7954a755a1279472ac5c4d41e9fbe239
James Lucktaylor <jlucktay+github@gmail.com>
2026-06-28 11:25:46 +0100

    refactor: 💡 Switch autoscaling node limit controls from per-zone to total

--- lean-gke/node_pool.tf
+++ lean-gke/node_pool.tf
@@ -13,8 +13,8 @@ resource "google_container_node_pool" "main" {
   autoscaling {
     location_policy = "BALANCED"
 
-    min_node_count = 1
-    max_node_count = 3
+    total_min_node_count = 1
+    total_max_node_count = 3
   }
 
   management {
